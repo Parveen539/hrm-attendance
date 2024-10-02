9dddcf72b4590b1c3e9019ca2e4e2c7f32a47fec
Sadiquehmd <Sadiquehmd29@gmail.com>
2024-10-02 15:54:13 +0530

    update login route and controller

--- backend/src/routes/admin/validate.routes.js
+++ backend/src/routes/admin/validate.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { employeeRegister, employeeRetrieveAll, employeeRetrieveByID, employeeUpdateByID } from '../../controllers/admin/employee.controller.js';
-import { changePassword, loginCheck } from "../../controllers/admin/login.controller.js";
+import { changePassword, forgetPassword, loginCheck, otpCheck, resetPassword } from "../../controllers/admin/login.controller.js";
 import { checkLogin } from "../../middlewares/checkLogin.js";
 
 const valRoute = Router();
 // Save Employee
 valRoute.route("/change-password/").post(checkLogin,changePassword);
+valRoute.route("/forget-password/").post(forgetPassword);
+valRoute.route("/verify-otp/").post(otpCheck);
+valRoute.route("/reset-password/").post(resetPassword);
 valRoute.route("/isValid/").post(loginCheck);
 
 
