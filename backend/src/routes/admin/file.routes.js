fc8553f21a4a95afc2987629371de7cd61c9a19b
rajan <rajansahu.vedsar@gmail.com>
2024-10-02 17:04:14 +0530

    error solved

--- backend/src/routes/admin/file.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { dbDownload } from "../../controllers/download.controller";
-const fileRouter=Router();
-
-fileRouter.route("/database/").post(dbDownload)
-
-export default fileRouter;
